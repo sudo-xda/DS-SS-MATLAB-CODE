67fe0c9975619f63e7f0d1051ee9572f499e529e
Jigyansha Jeevan Rout <62542350+sudo-xda@users.noreply.github.com>
2022-02-07 11:11:17 +0530

    Create DSSSMatlabCode.m

---
+++ DSSSMatlabCode.m
@@ -0,0 +1,42 @@
+clc, clear all, close all;
+Data=[1 0 1 0 0 1 0 1 1 1 0 0];
+Data=rectpulse(Data,1);
+G=length(Data);
+  
+sd1 =[1 1 1 1];            
+ PN1=[];                       
+ for j=1:G        
+     PN1=[PN1 sd1(4)];
+     if sd1(1)==sd1(2)
+         temp1=0;
+     else temp1=1;
+     end
+     sd1(1)=sd1(2);
+     sd1(2)=sd1(3);
+     sd1(3)=sd1(4);
+     sd1(4)= temp1; 
+ end 
+n=length(PN1);
+t=1:1:n;
+
+sp=xor(Data,PN1);
+sp=double(sp);
+% comp_sig=sp;
+snr=20;
+comp_sig=awgn(sp,snr);
+comp_sig(comp_sig(:)<0)=0;
+comp_sig1=logical(comp_sig);
+ds=xor(comp_sig1,PN1);
+figure(1)
+subplot(2,2,1);
+plot(Data); 
+title('Message Signal'); 
+subplot(2,2,2);
+plot(PN1);
+title('PN Sequence');
+subplot(2,2,3);
+plot(sp);
+title('Spreaded')
+subplot(2,2,4);
+plot(t,ds);
+title('DeSpreaded')
